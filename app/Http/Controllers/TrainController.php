7e95bfc69c9c5a257eb361bcb29c711e796554b6
Beniamino-Soccio <beniamino43@gmail.com>
2024-10-28 10:10:44 +0100

    fixed query

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class TrainController extends Controller
 {
     public function index(){
-        $trains = train::where("Data_di_partenza", "2024-10-25");
+        $trains = train::where("Data_di_partenza", "2024-10-25")->get();
         return view("trains.index", compact("trains"));
     }
 }
